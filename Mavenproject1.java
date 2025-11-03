d279ad980dbce84b5f68faed30252383b2482347
Caio Henrique de Oliveira <159477545+CaioHenrique09@users.noreply.github.com>
2025-11-03 10:24:43 -0300

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author alunolab09
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
